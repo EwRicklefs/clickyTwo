5ed2df962cc8209b1858e4d7ec00f0d62b5970af
EwRicklefs <ew.ricklefs@gmail.com>
2019-04-26 14:56:35 -0700

    Updates

---
+++ precache-manifest.d2a74195f964a7eff169889b4c7222bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a8ce51913e353b6ab625221972f66a",
+    "url": "/clickyTwo/index.html"
+  },
+  {
+    "revision": "2467b1d1602677856a99",
+    "url": "/clickyTwo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickyTwo/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2467b1d1602677856a99",
+    "url": "/clickyTwo/static/js/main.8b77b9c7.chunk.js"
+  },
+  {
+    "revision": "87d3d5d49be61b1d1373",
+    "url": "/clickyTwo/static/js/runtime~main.a4da705f.js"
+  }
+]);
